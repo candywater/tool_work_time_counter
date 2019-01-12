5191f91c08130152cae83db6b429b5bc47256053
candywater <candywater@users.noreply.github.com>
2019-01-12 14:54:42 +0800

    todo: add dynamic adding buttons function

--- work_time_counter/MainWindow.xaml.cs
+++ work_time_counter/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace work_time_counter
 {
@@ -20,9 +22,73 @@ namespace work_time_counter
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Dictionary<string, DispatcherTimer> list = new Dictionary<string, DispatcherTimer>();
+        public int count = 0;
+        public bool start = false;
+        public DispatcherTimer dis_1;
+
         public MainWindow()
         {
             InitializeComponent();
+            GetTodayTime();
+        }
+
+        // https://stackoverflow.com/questions/16748371/how-to-make-a-wpf-countdown-timer
+        // https://docs.microsoft.com/ja-jp/dotnet/api/system.windows.threading.dispatchertimer?view=netframework-4.7.2
+        public DispatcherTimer mkDisTime()
+        {
+            var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            return dispatcherTimer;
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine(e);
+        }
+
+
+        /// <summary>
+        /// get time now event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TodayTime_Tick(object sender, EventArgs e)
+        {
+            clock.Content = DateTime.Now.ToString();
+        }
+        /// <summary>
+        /// get time now
+        /// </summary>
+        void GetTodayTime()
+        {
+            clock.Content = DateTime.Now.ToString();
+            var dis = mkDisTime();
+            dis.Tick += new EventHandler(TodayTime_Tick);
+            dis.Start();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            start = !start;
+
+            if (start)
+            {
+                dis_1 = mkDisTime();
+                dis_1.Tick += new EventHandler(TimeCounter);
+                ell1.Fill = Brushes.Red;
+                dis_1.Start();
+            }
+            else
+            {
+                System.Diagnostics.Debug.Assert(dis_1 != null);
+                ell1.Fill = Brushes.Green;
+                dis_1.Stop();
+            }
+        }
+        private void TimeCounter(object sender, EventArgs e)
+        {
+            tsk1.Content = new TimeSpan(0, 0, ++count);
         }
     }
 }
